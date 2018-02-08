f272e06ae4e2b41b4f89ada38f2ab8930b4e72ad
MishaGl <mishaglansev@gmail.com>
2018-02-08 20:03:24 +0300

    Начало сделано открытие-закрытие

--- js/pictures.js
+++ js/pictures.js
@@ -74,7 +74,7 @@
 
   // Находим блок с основным фото и показываем его
   var mainPic = document.querySelector('.gallery-overlay');
-  mainPic.classList.remove('hidden');
+  // mainPic.classList.remove('hidden');
 
   // Вставляем первый элемент из сгенерированного массива в основной блок
   function drawMainPic(arr) {
@@ -83,4 +83,110 @@
     mainPic.querySelector('.comments-count').textContent = arr[0].comments.length;
   }
   drawMainPic(arrPhotos);
+
+
+  // ================================================================
+  var uploadFile = document.querySelector('#upload-file');
+  var uploadOverlay = document.querySelector('.upload-overlay');
+  // var loadEffect = document.querySelector('.upload-effect');
+  var uploadFormCancel = document.querySelector('.upload-form-cancel');
+
+  function openPopup() {
+    uploadOverlay.classList.remove('hidden');
+    document.addEventListener('keydown', onPopupEscPress);
+  }
+
+  function closePopup() {
+    uploadOverlay.classList.add('hidden');
+    document.removeEventListener('keydown', onPopupEscPress);
+  }
+
+  function onPopupEscPress(evt) {
+    if (evt.keyCode === 27) {
+      closePopup();
+    }
+  }
+
+  // function onUploadEnterPress(evt) {
+  //   if (evt.keyCode === 13) {
+  //     openPopup();
+  //   }
+  // }
+
+  uploadFile.addEventListener('change', openPopup);
+  uploadFormCancel.addEventListener('click', closePopup);
+  // uploadFile.addEventListener('keydown', onUploadEnterPress);
+
+
+  var buttonDec = document.querySelector('.upload-resize-controls-button-dec');
+  var buttonInc = document.querySelector('.upload-resize-controls-button-inc');
+  var controlValue = document.querySelector('.upload-resize-controls-value');
+
+
+  function setControlValue() {
+    switch (controlValue.value) {
+      case '25%':
+        controlValue.value = '50%';
+        getTransformImage(effectImage, 0.5);
+        break;
+      case '50%':
+        controlValue.value = '75%';
+        getTransformImage(effectImage, 0.75);
+        break;
+      case '75%':
+        controlValue.value = '100%';
+        getTransformImage(effectImage, 1);
+        break;
+    }
+  }
+
+
+  function getTransformImage(elementOfTransform, valueOfScale) {
+    elementOfTransform.style.transform = 'scale(' + (valueOfScale) + ')';
+  }
+
+  function setControlValue2() {
+    switch (controlValue.value) {
+      case '50%':
+        controlValue.value = '25%';
+        getTransformImage(effectImage, 0.25);
+        break;
+      case '75%':
+        controlValue.value = '50%';
+        getTransformImage(effectImage, 0.5);
+        break;
+      case '100%':
+        controlValue.value = '75%';
+        getTransformImage(effectImage, 0.75);
+        break;
+    }
+  }
+
+  buttonInc.addEventListener('click', setControlValue);
+  buttonDec.addEventListener('click', setControlValue2);
+
+  var effectImage = document.querySelector('.effect-image-preview');
+
+
+  // функция для добавления классов фильтров
+  // function getFilterClass() {
+  //
+  // }
+
+  var arr1 = ['upload-effect-none', 'upload-effect-chrome', 'upload-effect-sepia', 'upload-effect-marvin', 'upload-effect-phobos', 'upload-effect-heat'];
+  var arr2 = ['effect-none', 'effect-chrome', 'effect-sepia', 'effect-marvin', 'effect-phobos', 'effect-heat'];
+  var uploadEffectControls = document.querySelector('.upload-effect-controls');
+
+
+  function functionName2(evt) {
+    var target = evt.target;
+    for (var i = 0; i < arr1; i++) {
+      if (target.id === arr1[i]) {
+        effectImage.classList.add('' + arr2[i]);
+      }
+    }
+  }
+
+ uploadEffectControls.addEventListener('click', functionName2);
+
 })();
