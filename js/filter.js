2e8cad0f156106dffecced9336d56ea0068a630e
MishaGl <mishaglansev@gmail.com>
2018-02-24 00:50:52 +0300

    abkmnhs xthyjdbr

---
+++ js/filter.js
@@ -0,0 +1,32 @@
+// 'use strict';
+// (function () {
+//
+//
+//   var filters = document.querySelector('.filters');
+//   var filterDiscussed = document.querySelector('#filter-discussed');
+//   filters.classList.remove('filters-inactive');
+//
+//   function functionName(a, b) {
+//     return a.comments.length - b.comments.length;
+//   }
+//
+//
+//
+//   function drawPictureR(arr) {
+//     arr.forEach(function (elemPhotoArr) {
+//       window.pictures.pictureFragment.appendChild(renderPicture(elemPhotoArr));
+//       window.previewpictures.pictureContainer.appendChild(pictureFragment);
+//     });
+//   }
+//
+//   filterDiscussed.addEventListener('click', function () {
+//       var vv = [];
+//       vv = window.pictures;
+//       vv.sort(functionName);
+//       drawPictureR(vv)
+//     console.log(vv);
+//       debugger;
+//   });
+//
+//
+// })();
--- js/pictures.js
+++ js/pictures.js
@@ -28,4 +28,62 @@
 
   // Загрузка с сервера
   window.backend.load(drawPicture, window.backend.errorHandler);
+
+  function drawPictureR(arr) {
+    arr.forEach(function (elemPhotoArr) {
+      pictureFragment.appendChild(renderPicture(elemPhotoArr));
+      window.previewpictures.pictureContainer.appendChild(pictureFragment);
+    });
+  }
+
+  // ========================================================================
+  var filters = document.querySelector('.filters');
+  var filterDiscussed = document.querySelector('#filter-discussed');
+  filters.classList.remove('filters-inactive');
+
+  var pic = document.querySelector('.pictures');
+
+  function functionName(a, b) {
+    return a.comments.length - b.comments.length;
+  }
+
+  function reset(elem) {
+    elem.innerHTML = '';
+  }
+
+  filterDiscussed.addEventListener('click', function () {
+    var vv = window.pictures.slice(0);
+    vv.sort(functionName);
+    reset(pic);
+    drawPictureR(vv);
+    console.log(vv);
+  });
+
+
+  var filterPopular = document.querySelector('#filter-popular');
+
+  filterPopular.addEventListener('click', function () {
+    console.log(window.pictures);
+    reset(pic);
+    drawPictureR(window.pictures);
+  });
+
+  var filterRandom = document.querySelector('#filter-random');
+
+  function compareRandom(a, b) {
+    if (a > b) {
+      return 1;
+    }
+    if (a < b) {
+      return -1;
+    }
+    return Math.random() - 0.5;
+  }
+
+  // filterRandom.addEventListener('click', function () {
+  //   var vvv = window.pictures.slice(0).sort(compareRandom).splice(0, 10);
+  //   console.log(vvv);
+  //   reset(pic);
+  //   drawPictureR(vvv);
+  // });
 })();
