2582d05c9f9f77946dc4d95699992edd98bd1d41
MishaGl <mishaglansev@gmail.com>
2018-02-26 18:32:04 +0300

    Исправления начало

--- js/backend.js
+++ js/backend.js
@@ -9,13 +9,18 @@
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
+    // xhr.addEventListener('load', function () {
+    //   if (xhr.status === statusOk) {
+    //     onLoad(xhr.response);
+    //   } else {
+    //     onError(xhr.response);
+    //   }
+    // });
+
     xhr.addEventListener('load', function () {
-      if (xhr.status === statusOk) {
-        onLoad(xhr.response);
-      } else {
-        onError(xhr.response);
-      }
+      return (xhr.status === statusOk) ? onLoad(xhr.response) : onError(xhr.response);
     });
+
     xhr.addEventListener('error', function () {
       onError('Произошла ошибка соединения');
     });
